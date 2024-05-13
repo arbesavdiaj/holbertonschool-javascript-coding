75ddb2b0378d77fcfc33e420066c9c5005cf24e3
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-05-13 09:57:59 +0200

    write a script that reads and prints the content of a file

---
+++ 0-readme.js
@@ -0,0 +1,10 @@
+/*eslint-disable*/
+const fs = require("fs");
+
+fs.readFile("0-readme.js", "utf-8", (err, data) => {
+  if (err) {
+    console.error("Error reading file:", err);
+    return;
+  }
+  console.log("File contents:", data);
+});
