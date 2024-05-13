157a76823e4ef61bf4aeb302c4ed8268905c0de0
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-05-13 10:18:38 +0200

    write a script that writes a string to a file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const fs = require("fs");
+
+const fs = require('fs');
 const arg2 = process.argv[2];
 const arg3 = process.argv[3];
 
-fs.writeFile(arg2, arg3, (err) => {
+fs.writeFile(arg2, arg3, err => {
   if (err) {
     console.log(err);
   }
