4119a13a211525d06bb7eae0c6fe8a932e93e4c7
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-05-13 10:04:25 +0200

    write a script that reads and prints the content of a file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 const fs = require("fs");
 
-fs.readFile("0-readme.js", "utf-8", (err, data) => {
-  if (err) {
-    console.error("Error reading file:", err);
-    return;
-  }
-  console.log("File contents:", data);
+fs.readFile(process.argv[2], "utf-8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
 });
