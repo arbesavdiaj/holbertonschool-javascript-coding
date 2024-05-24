de561f623c1915b4fcfa84be58c9dfbb3168c245
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-05-24 11:12:22 +0200

    add

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,20 +1,29 @@
 const http = require('http');
+const url = require('url');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') res.write('Hello Holberton School!');
-  if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    try {
-      const data = await countStudents(process.argv[2]);
-      res.end(`${data.join('\n')}`);
-    } catch (error) {
-      res.end(error.message);
-    }
+const app = http.createServer((require, result) => {
+  const path = url.parse(require.url).pathname;
+  if (path === '/') {
+    result.end('Hello Holberton School!');
+  } else if (path === '/students') {
+    result.write('This is the list of our students\n');
+    countStudents(process.argv[2])
+      .then((data) => {
+        result.write(`Number of students: ${data.students.length}\n`);
+        for (const field in data.subjects) {
+          if (Object.prototype.hasOwnProperty.call(data.subjects, field)) {
+            result.write(`Number of students in ${field}: ${data.subjects[field].length}. List: ${data.subjects[field].join(', ')}\n`);
+          }
+        }
+        result.end();
+      })
+      .catch((error) => {
+        result.end(error.message);
+      });
   }
-  res.end();
 });
+
 app.listen(1245);
+
 module.exports = app;
