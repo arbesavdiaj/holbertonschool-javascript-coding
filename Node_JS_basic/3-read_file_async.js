9ae4c3e65d65adc77fc58aa5ddefc20e4fb5b642
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-05-27 09:29:07 +0200

    add new code

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,36 +1,30 @@
 const fs = require('fs');
 
-function countStudents(path) {
-  const promise = (res, rej) => {
-    fs.readFile(path, 'utf8', (error, data) => {
-      if (error) rej(Error('Cannot load the database'));
-      const messages = [];
-      let message;
-      const content = data.toString().split('\n');
-      let students = content.filter((item) => item);
-      students = students.map((item) => item.split(','));
-      const nStudents = students.length ? students.length - 1 : 0;
-      message = `Number of students: ${nStudents}`;
-      console.log(message);
-      messages.push(message);
-      const subjects = {};
-      for (const i in students) {
-        if (i !== 0) {
-          if (!subjects[students[i][3]]) subjects[students[i][3]] = [];
-          subjects[students[i][3]].push(students[i][0]);
+function countStudents(argPath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(argPath, 'utf8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      lines.splice(0, 1);
+      const students = lines.map((line) => {
+        const [firstname, lastname, age, field] = line.split(',');
+        return {
+          firstname, lastname, age, field,
+        };
+      });
+      const subjects = students.reduce((acc, student) => {
+        if (!acc[student.field]) {
+          acc[student.field] = [];
         }
-      }
-      delete subjects.subject;
-      for (const key of Object.keys(subjects)) {
-        message = `Number of students in ${key}: ${
-          subjects[key].length
-        }. List: ${subjects[key].join(', ')}`;
-        console.log(message);
-        messages.push(message);
-      }
-      res(messages);
+        acc[student.field].push(student.firstname);
+        return acc;
+      }, {});
+      resolve({ students, subjects });
     });
-  };
-  return new Promise(promise);
+  });
 }
+
 module.exports = countStudents;
