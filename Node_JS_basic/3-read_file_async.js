62fb73f976b9595f0033761e13e1c675260430f0
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-05-27 09:33:50 +0200

    add new code

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -3,13 +3,13 @@ const fs = require('fs').promises;
 async function countStudents(path) {
   try {
     const data = await fs.readFile(path, 'utf8');
-    const lines = data.split('\n').filter(line => line.trim() !== '');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
 
     if (lines.length === 0) {
       throw new Error('Cannot load the database');
     }
 
-    const students = lines.slice(1).map(line => line.split(','));
+    const students = lines.slice(1).map((line) => line.split(','));
 
     const fields = {};
     for (const student of students) {
